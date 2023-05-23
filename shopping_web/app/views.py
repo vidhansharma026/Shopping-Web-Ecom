ffa6a73a7a3e0c7c47e2714713bd33bf1c785500
SHARM2575A <vidhansharma026@gmail.com>
2023-05-23 14:40:30 +0530

    'products_dynamic'

---
+++ shopping_web/app/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render,redirect
+from . models import *
+from django.views import View
+
+# def home(request):
+#  return render(request, 'app/home.html')
+
+def product_detail(request):
+ products = Product.objects.all()       
+ context = {'products' : products}
+ return render(request, 'app/productdetail.html',context)
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def profile(request):
+ return render(request, 'app/profile.html')
+
+def address(request):
+ return render(request, 'app/address.html')
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+def change_password(request):
+ return render(request, 'app/changepassword.html')
+
+def electronics(request):
+ return render(request, 'app/electronics.html')
+
+def login(request):
+ return render(request, 'app/login.html')
+
+def customerregistration(request):
+ return render(request, 'app/customerregistration.html')
+
+def checkout(request):
+ return render(request, 'app/checkout.html')
+
+
+class ProductView(View):
+ def get(self, request):
+  topwears = Product.objects.filter(category = 'TW')
+  bottomwears = Product.objects.filter(category = 'BW')
+  electronics = Product.objects.filter(category = 'E')
+  accesories = Product.objects.filter(category = 'A')
+  return render(request, 'app/home.html',{'topwears':topwears, 'bottomwears':bottomwears, 'electronics':electronics, 'accesories':accesories})
