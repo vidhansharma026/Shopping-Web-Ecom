4369badb0c517bf1582f6020d06a4cc8ed16884e
SHARM2575A <vidhansharma026@gmail.com>
2023-05-26 20:49:29 +0530

    'add-to-cart'

--- shopping_web/app/urls.py
+++ shopping_web/app/urls.py
@@ -5,8 +5,11 @@ from django.conf import settings
 urlpatterns = [
     # path('', views.home),
     path('', views.ProductView.as_view(), name='home'),
-    path('product-detail/', views.product_detail, name='product-detail'),
-    path('cart/', views.add_to_cart, name='add-to-cart'),
+    # path('product-detail/', views.product_detail, name='product-detail'),
+    path('product-detail/<int:pk>',
+         views.ProductDetailView.as_view(), name='product-detail'),
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
